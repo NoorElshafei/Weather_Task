f4776cd61ba94b819229ac62f54e8224bb31969a
Noor Elshafei <noorelshafei@paymob.com>
2024-04-20 07:42:44 +0200

    making some changes at gradle and settings.gradle.kts and adding libs.versions.toml

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+@file:Suppress("UnstableApiUsage")
+
+include(":core:utils")
+
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+
+    }
+}
+
+rootProject.name = "Weather Task"
+include(":app")
+include(":core")
+include(":core:Network")
+
