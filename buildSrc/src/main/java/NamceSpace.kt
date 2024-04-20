4c7c6714ca20b8a088aea91069bd279822ad21a5
Noor Elshafei <noorelshafei@paymob.com>
2024-04-20 03:09:05 +0200

    adding buildSrc module

---
+++ buildSrc/src/main/java/NamceSpace.kt
@@ -0,0 +1,17 @@
+object NamceSpace {
+
+    const val applicationId = "com.baims.weathertask"
+
+    object Core {
+        private const val core = "com.baims.core"
+        const val network = "$core.network"
+        const val utils = "$core.utils"
+        const val ui = "$core.ui"
+        const val uiTest = "$core.uiTest"
+    }
+
+    object Feature {
+        private const val feature = "com.baims.feature"
+        const val articles = "$feature.articles"
+    }
+}
